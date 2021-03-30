963cf446deb589daaa73343a25fec211f1c5209c
KRIS0123 <kris12112001@gmail.com>
2021-03-30 12:35:26 +0100

    Small changes

--- Main.java
+++ Main.java
@@ -1,33 +1,26 @@
 import java.io.*;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File readFile = new File("I:\\TxtModifier\\src\\ReadHere.txt");
+        File readKeys = new File("I:\\TxtModifier\\src\\Keys.txt");
         FileWriter fileWriter = new FileWriter("I:\\TxtModifier\\src\\WriteHere.txt");
+        FileWriter keyWriter = new FileWriter("I:\\TxtModifier\\src\\Keys.txt", true);
+        FileWriter exceptionWriter = new FileWriter("I:\\TxtModifier\\src\\ManualExceptionHandler.txt");
+        FileWriter pdfWriter = new FileWriter("I:\\TxtModifier\\src\\PDFHandler.txt", true);
 
         Scanner scanner = new Scanner(readFile);
+        List<String> keys = getKeys(readKeys);
+        Map<String, String> urlToTitleMap = new LinkedHashMap<>();
 
-        Map<String, String> urlToTitleMap = new HashMap<>();
-
-        Integer hashtagCount = 0;
-        Integer urlRepetitionsCount = 0;
-        Integer forbiddenPageCount = 0;
-        Integer encounteredFileCount = 0;
         while (scanner.hasNextLine()) {
             String row = scanner.nextLine();
-            if (row.contains("#")) {
-                String[] tokens = row.split("#");
-                hashtagCount++;
-                row = tokens[0];
-            }
-            if (urlToTitleMap.containsKey(row)) {
-                urlRepetitionsCount++;
-            } else {
+
+            if (!keys.contains(row.toLowerCase())) {
+                keys.add(row.toLowerCase());
+                keyWriter.write(row.toLowerCase() + '\n');
                 try {
                     URL url = new URL(row);
                     InputStream is = url.openStream();
@@ -39,28 +32,47 @@ public class Main {
                     is.close();
                     try {
                         urlToTitleMap.put(row, getTitleFromURL(String.valueOf(buffer)));
+                        System.out.println("Success");
                     }catch (Exception e){
-                        urlToTitleMap.put(row, "PROBLEM GETTING TITLE: PROBABLY A DOCUMENT");
-                        encounteredFileCount++;
+                        System.out.println("Fail");
+                        if(row.toLowerCase().contains("pdf") || row.toLowerCase().contains("doc") || row.toLowerCase().contains("xls")){
+                            pdfWriter.write(row + '\n');
+                        }else {
+                            exceptionWriter.write(row + '\n');
+                        }
                     }
                 }catch (IOException e){
                     urlToTitleMap.put(row, "PROBLEM LOADING URL: ERROR 403 FORBIDDEN");
-                    forbiddenPageCount++;
+                    System.out.println("Fail");
                 }
+            }else {
+                System.out.println("Repetition");
             }
         }
 
-        scanner.close();
         for (String key:urlToTitleMap.keySet()) {
             fileWriter.write(urlToTitleMap.get(key) + " - " + key + "\n");
         }
+        scanner.close();
+        pdfWriter.close();
+        exceptionWriter.close();
+        keyWriter.close();
         fileWriter.close();
     }
 
-    public final static String getTitleFromURL(String httpCode) {
+    public static String getTitleFromURL(String httpCode) {
         String[] tokens = httpCode.split("<\\btitle\\b>");
         String[] tokens2 = tokens[1].split("</\\btitle\\b>");
         return tokens2[0];
     }
 
+    public static List<String> getKeys(File readKeys) throws FileNotFoundException {
+        Scanner scanner = new Scanner(readKeys);
+        List<String> keys = new LinkedList<>();
+        while(scanner.hasNextLine()){
+            keys.add(scanner.nextLine().toLowerCase());
+        }
+        scanner.close();
+        return keys;
+    }
 }
